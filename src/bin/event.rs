d5f9f9c040d1aab0b34ec4ba2deb8c9e93821adf
Michael Sutton <msutton@cs.huji.ac.il>
2026-07-30 03:01:27 +0300

    fmt

--- src/bin/event.rs
+++ src/bin/event.rs
@@ -1,7 +1,5 @@
 use argent::build_file;
-use argent_runtime::{
-    EntryCall, TxBuilder, TxContext, args as entry_args, state as actor_state,
-};
+use argent_runtime::{EntryCall, TxBuilder, TxContext, args as entry_args, state as actor_state};
 use argent_template::{DemoResult, demo_keys, demo_outpoint, print_tx_summary};
 use kaspa_consensus_core::{Hash, tx::CovenantBinding};
 
@@ -29,39 +27,18 @@ fn main() -> DemoResult<()> {
     let (_buyer, buyer_pk) = demo_keys(23);
 
     let cov_id = Hash::from_u64_word(257);
-    let event_before =
-        actor_state! { remaining_tickets: 7, serial: 56, price: 1000 };
-    let event_after =
-        actor_state! { remaining_tickets: 6, serial: 57, price: 1000 };
+    let event_before = actor_state! { remaining_tickets: 7, serial: 56, price: 1000 };
+    let event_after = actor_state! { remaining_tickets: 6, serial: 57, price: 1000 };
     let new_ticket = actor_state! { owner: buyer_pk.clone(), serial: 56 };
     let buy_args = entry_args![buyer_pk.clone()];
 
     // get the utxo of the event from a kaspa node or from storage
     let outpoint = demo_outpoint(67, 0);
-    let utxo = builder.covenant_utxo(
-        "Event",
-        event_before.clone(),
-        500,
-        0,
-        false,
-        Some(cov_id),
-    )?;
+    let utxo = builder.covenant_utxo("Event", event_before.clone(), 500, 0, false, Some(cov_id))?;
 
     let ctx = TxContext::new()
-        .actor_input(
-            "Event",
-            event_before,
-            EntryCall::new("buy").args(buy_args),
-            outpoint,
-            utxo,
-            0,
-        )
-        .actor_output(
-            "Event",
-            event_after,
-            CovenantBinding::new(0, cov_id),
-            1500,
-        )
+        .actor_input("Event", event_before, EntryCall::new("buy").args(buy_args), outpoint, utxo, 0)
+        .actor_output("Event", event_after, CovenantBinding::new(0, cov_id), 1500)
         .actor_output("Ticket", new_ticket, CovenantBinding::new(0, cov_id), 1);
 
     let tx = builder.build(&ctx)?;
